d186b6eb9388ff416ce1fdeee52025a5a15bf093
cnickbradford <cnickbradford@gmail.com>
2023-08-03 18:18:40 -0500

    Completed building components and added footer

--- perinatal-alliance/src/App.jsx
+++ perinatal-alliance/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header-and-Footer/Header";
+import Home from "./Components/Home/Home";
+import AboutPage from "./Components/About/AboutPage";
+import FAQPage from "./Components/FAQ/FAQPage";
+import ContactPage from "./Components/Contact/ContactPage";
+import Footer from "./Components/Header-and-Footer/Footer";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <>
+        <div>
+          <Header />
+        </div>
+        <div className="body">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/FAQ" element={<FAQPage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ perinatal-alliance/src/Components/Header-and-Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h4 className="footer-title">
+        Copyright © 2023 Perinatal alliance - All Rights Reserved.
+      </h4>
+    </div>
+  );
+};
+
+export default Footer;
